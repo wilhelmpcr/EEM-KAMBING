6466490b676c9c65bddc2c9e573608fed24587fa
muthiaadeis <muthia23ti@mahasiswa.pcr.ac.id>
2025-09-23 07:43:51 +0700

    setup awal laravel

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+Route::get('/nama/{eem}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '2457301152') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show'])->name('user.show');
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
